2ec1130bf7bb3f29d78a47dbd4a2b8d8e7a06288
3acdigital <3acdigital@gmail.com>
2024-02-13 14:12:44 +0000

    - Added the config file for storing the credentials for a apis

---
+++ config/apis.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Apis
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party apis.
+    |
+    */
+
+    'punk_api' => [
+        'endpoint' => env('PUNK_API_ENDPOINT', 'https://api.punkapi.com/v2/')
+    ],
+    
+];
